a754dc7485e63f844d5f8876388dee99b9017d70
captaiiinsolo <solo.d.santos@gmail.com>
2022-12-22 04:22:02 -0800

    add prompts and confirms

--- Develop/script.js
+++ Develop/script.js
@@ -10,6 +10,50 @@ function writePassword() {
 
 }
 
+function pleaseTryAgain() {
+  var tryAgain = alert("Password must be between 8 and 128 characters long. Please try again.");
+}
+
+function caseSelections() {
+  var characterCaseLower = confirm("Would you like to include lowercase letters?");
+  var characterCaseUpper = confirm("Would you like to include uppercase letters included?");
+  var characterSpecial = confirm("Would you like to include special characters?");
+  var characterNumbers = confirm("Would you like to include numbers?");
+    if (characterCaseLower !== true) {
+      return characterCaseUpper;
+    }
+    else if (characterCaseLower === true) {
+      characterCaseUpper;
+    }
+
+    else if (characterCaseUpper !== true) {
+      return characterSpecial;
+    }
+
+    else if (characterSpecial === true) {
+      characterNumbers;
+    }
+}
+
+
+// Generate new password
+function generatePassword() {
+  var countLow = 8;
+  var countHigh = 128;
+  var charAvailableAlpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var charAvailableNum = "0123456789"
+  var charAvailableSpecial = "!@#$%^&*(-_<>,."
+  var characterCountPrompt = prompt("How many characters would you like your password to contain?", "8-128");
+    if (characterCountPrompt < countLow || characterCountPrompt > countHigh) {
+      pleaseTryAgain();
+    }
+
+    else if (characterCountPrompt >= countLow || characterCountPrompt <= countHigh) {
+      caseSelections();
+    }
+   
+  }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
